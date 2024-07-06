70daa1a8b96cc45b4878d07c0ac9efb7835e0568
yu <fleur.jv@gmail.com>
2024-07-06 11:11:02 +0100

    start to use pytest-playwright fixtures

--- test_todo.py
+++ test_todo.py
@@ -1,15 +1,9 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
-
-
-def test_add_todo(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context()
-    page = context.new_page()
+def test_add_todo(page):
     page.goto("https://demo.playwright.dev/todomvc/#/")
     page.get_by_placeholder("What needs to be done?").click()
     page.get_by_placeholder("What needs to be done?").fill("Создать первый сценарий")
     page.get_by_placeholder("What needs to be done?").press("Enter")
 
     # ---------------------
-    context.close()
-    browser.close()
+    page.close()
+    page.close()
